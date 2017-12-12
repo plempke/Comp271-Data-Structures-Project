030e653877be791211698cba33d7947e7e15ae8c
Nick Taylor <31541603+ntaylor5@users.noreply.github.com>
2017-12-12 02:39:29 -0600

    Restructured Menu
    Changed the menu from option first, deck choice second
    to deck choice first, option second.

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -4,62 +4,59 @@ import java.util.*;
 
 public class Deck {
 
-    Scanner kbDeck = new Scanner(System.in);
-    int deckSize;
-    String deckName;
-    String quest;
-    String ans;
-    Queue<FlashCard> myDeck = new LinkedList<FlashCard>();
+    Scanner kbDeck = new Scanner(System.in);        // scanner for this class
+    int deckSize;                                   // integer for the number of cards in the deck
+                                                    // this integer is not actually the number of cards in the deck but is equal to it instead.
+                                                    // basically, when the number of cards in the deck changes from the pop method, this number does not actually change and allows the for loops to run correctly
+    String deckName;                                // name of the deck
+    String quest;                                   // top side of the deck (question)
+    String ans;                                     // bottom side of the deck (answer)
+    Queue<FlashCard> myDeck = new LinkedList<FlashCard>();  // queue to display cards in the order they are entered
 
-    public Deck(String name){
+    public Deck(String name){   // constructor using the argument from line 33 in Main.java
         deckName = name;
-        System.out.println("Enter an integer deck size:");
+        System.out.println("Enter an integer deck size:");  // rather than ask the user if they want to enter a new card every time, we ask ahead of time what the deck size for faster, more streamlined deck creation
         deckSize = kbDeck.nextInt();
         kbDeck.nextLine();
-        FlashCard[] fcArray = new FlashCard[deckSize];
+        FlashCard[] fcArray = new FlashCard[deckSize];  // creates an array of flashcards the size of the user inputted value
 
-        for (int i=0; i<deckSize; i++) {
-            System.out.println("\nCard " + (i+1) + "\n");
+        for (int i=0; i<deckSize; i++) {    // creates one card and repeats an amount of times equal to the user input for deck size
+            System.out.println("\nCard " + (i+1));
             System.out.println("enter the top side of the card:");
             quest = kbDeck.nextLine();
             System.out.println("enter the bottom side of the card:");
             ans = kbDeck.nextLine();
-            fcArray[i] = new FlashCard(quest, ans);
-            myDeck.add(fcArray[i]);
+            fcArray[i] = new FlashCard(quest, ans); // constructor to create a flashcard (9 line class at the bottom of this file)
+            myDeck.add(fcArray[i]); // adds the flashcard to the queue
         }
     }
 
-
-    // The simple bullshit version of Stduy
-
-    public void studyByQuestion(){
+    public void studyByQuestion(){  // runs through the queue displaying the top side of the card
 
         FlashCard temp;
-
-        for (int i=0; i<deckSize; i++) {
-            temp = myDeck.poll();
+        for (int i=0; i<deckSize; i++) {    // this is where deckSize's unchanging value is important as the deck's actual size DOES change during this loop
+            temp = myDeck.poll();   // pulls the card out of the front of the queue (this removes it from the queue)
             System.out.println("Top:");
             System.out.println(temp.question);
             kbDeck.nextLine();
             System.out.println("Bottom:");
             System.out.println(temp.answer + "\n");
-            myDeck.add(temp);
+            myDeck.add(temp);       // re-adds the pulled card to the back of the queue
 
         }
     }
 
-    public void studyByAnswer(){
+    public void studyByAnswer(){    // runs through the queue displaying the bottom side of the card
 
         FlashCard temp;
-
-        for (int i=0; i<deckSize; i++) {
-            temp = myDeck.poll();
+        for (int i=0; i<deckSize; i++) {    // this is where deckSize's unchanging value is important as the deck's actual size DOES change during this loop
+            temp = myDeck.poll();   // pulls the card out of the front of the queue (this removes it from the queue)
             System.out.println("Bottom:");
             System.out.println(temp.answer);
             kbDeck.nextLine();
             System.out.println("Top:");
             System.out.println(temp.question+ "\n");
-            myDeck.add(temp);
+            myDeck.add(temp);   // re-adds the pulled card to the back of the queue
         }
     }
 
@@ -93,11 +90,11 @@ public class Deck {
 
 
 
-class FlashCard {
-    String question;
-    String answer;
+class FlashCard {       // very simple custom object that just holds two strings representing two sides of a flashcard
+    String question;    // top side
+    String answer;      // bottom side
 
-    FlashCard (String q, String a) {
+    FlashCard (String q, String a) {    // constructor from line 29 of this file
         question = q;
         answer = a;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,120 +6,140 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int switchInt = 1;
-        int userInput;
-        String userConfirm;
-        Scanner kb = new Scanner(System.in);
-        Deck newDeck;
-        List<Deck> deckList = new ArrayList<Deck>();
-        String nameTemp;
+        int firstSwitchInt = 1;                     // create, select, end.
+        int secondSwitchInt;                        // Study by question/answer, shuffle, delete.
+        int userInput;                              // used for deck selection
+        String userConfirm;                         // Y/N string for confirmation
+        Scanner kb = new Scanner(System.in);        // Keyboard
+        Deck newDeck;                               // temporary storage for deck creation
+        Deck selectedDeck;                          // storage for selected deck sub-menu
+        List<Deck> deckList = new ArrayList<Deck>();   // list of decks
+        String nameTemp;                            // temporary string for the name of the new deck in deck creation
+
+
 
         System.out.println("Welcome to this Flashcard Application!");
         System.out.println("Start by creating a deck.");
 
-        do {
-            switch (switchInt) {
-                case 1: System.out.println("1");
-                    //Create a deck
+
+
+
+        do {    // This is a do statement so that we can push the user to create a deck right off the bat.
+            switch (firstSwitchInt) {
+
+                case 1: // Create a deck
+
+                    //System.out.println("1"); //Debug purposes
                     System.out.println("Enter deck name:");
                     nameTemp = kb.nextLine();
-                    newDeck = new Deck (nameTemp);
-                    deckList.add(newDeck);
+                    newDeck = new Deck(nameTemp);
+                    deckList.add(newDeck);  // creates a new deck with the name as an argument
                     break;
 
-                case 2: System.out.println("2");
-                    //Study Deck by question
-                    if (deckList.size()==0) {
-                        System.out.println("No deck is stored");
+                case 2: // Select a deck
+
+                    //System.out.println("2"); //Debug purposes
+                    if (deckList.size() == 0) {
+                        System.out.println("No deck is stored");    // this if statement is for the case that there is no deck
                         break;
                     }
+
                     else {
-                        System.out.println("Enter an integer to select a deck to study:\n");
-                        for (int i = 0; i<deckList.size(); i++) {
-                            System.out.println((i+1) + ". " + deckList.get(i).deckName);
+                        System.out.println("Enter an integer to select a deck:\n");
+                        for (int i = 0; i < deckList.size(); i++) {
+                            System.out.println((i + 1) + ". " + deckList.get(i).deckName);  // this for loop prints out all the deck names with a "#. " before them
                         }
                         userInput = kb.nextInt();
-                        if (userInput > deckList.size()) {
+
+                        if (userInput > deckList.size() || userInput <= 0) {  // this if statement runs if the user inputs a number higher than the number of decks or is less than or equal to 0
                             System.out.println("Selection not a valid deck.");
                             break;
                         }
-                        deckList.get(userInput-1).studyByQuestion();
+                        selectedDeck = deckList.get(userInput-1);   // takes the deck that the user selected and stores it in SelectedDeck
+
+
+
+
+                                // beginning of the sub-menu for the individual deck
+                        do {    // this is a do rather than a while so that when the user selects 5 to return to the main menu, this loop will still run correctly upon returning
+
+                            System.out.println("\n1. Study " + selectedDeck.deckName + " by question");
+                            System.out.println("2. Study " + selectedDeck.deckName + " by answer");
+                            System.out.println("3. Shuffle " + selectedDeck.deckName);
+                            System.out.println("4. Delete " + selectedDeck.deckName);
+                            System.out.println("5. Return to main menu\n");
+
+                            secondSwitchInt = kb.nextInt();
+                            kb.nextLine();
+
+                            switch (secondSwitchInt) {
+                                case 1:
+                                    //Study by question
+                                    selectedDeck.studyByQuestion(); // runs the studyByQuestion method
+                                    break;
+
+                                case 2:
+                                    //Study by answer
+                                    selectedDeck.studyByAnswer();   // runs the studyByAnswer method
+                                    break;
+
+                                case 3:
+                                    //Shuffle Deck
+                                    //selectedDeck.shuffle
+                                    System.out.println("Shuffle is not yet implemented");   // Debug purposes
+                                    break;
+
+                                case 4:
+                                    //Delete Deck
+                                    System.out.println("Are you sure you wish to delete " + selectedDeck.deckName + "? (Y/N)"); // The confirmation inquiry exists for the case that the user hits 4 on accident (user health)
+                                    userConfirm = kb.nextLine();
+                                    if (userConfirm.equalsIgnoreCase("y")) {
+                                        deckList.remove((userInput - 1));   // deleting the deck stored in selectedDeck does not actually remove it from the list as selectedDeck is just copy of the deck from the list
+                                                                            // one benefit of using a dummy deck for these operations is that the original order is stored in the event that the user wants to return to the original deck order after shuffling.
+                                        System.out.println("Deck deleted.");    //debug purposes
+                                    }
+                                    else {
+                                        System.out.println("Deck not deleted.");    //debug purposes
+                                    }
+                                    break;
+
+                                case 5:
+                                    // return to main menu
+                                    // it's more important that secondSwitchInt = 5 for the while statement below than the actual case statement here
+                                    break;
+
+                                default:    // if the user inputs a number that is not 1-5
+                                    System.out.println("Invalid choice");
+
+                            }
+                        } while (secondSwitchInt != 5); // if the user imputs 5 for the case statement, this while will break
+                        System.out.println("Returning to main menu.");  // debug purposes
                     }
                     break;
 
-                case 3: System.out.println("3");
-                    //Study Deck by answer
-                    if (deckList.size()==0) {
-                        System.out.println("No deck is stored");
-                        break;
-                    }
-                    else {
-                        System.out.println("Enter an integer to select a deck to study:\n");
-                        for (int i = 0; i<deckList.size(); i++) {
-                            System.out.println((i+1) + ". " + deckList.get(i).deckName);
-                        }
-                        userInput = kb.nextInt();
-                        if (userInput > deckList.size()) {
-                            System.out.println("Selection not a valid deck.");
-                            break;
-                        }
-                        deckList.get(userInput-1).studyByAnswer();
-                    }
+
+
+
+                case 3:
+                    // end program
+                    // it's more important that firstSwitchInt = 3 for the while statement below than the actual case statement here
                     break;
 
-                case 4: System.out.println("4");
-                    //Delete a deck
-                    //Probably more useful if we implement multiple deck functionality
-                    System.out.println("Enter an integer to choose a deck to delete:\n");
-                    for (int i = 0; i<deckList.size(); i++) {
-                        System.out.println((i+1) + ". " + deckList.get(i).deckName);
-                    }
-                    userInput = kb.nextInt();
-                    kb.nextLine();
-                    if (userInput > deckList.size()) {
-                        System.out.println("Selection not a valid deck.");
-                        break;
-                    }
-                    System.out.println("Are you sure you wish to delete " + deckList.get(userInput-1).deckName + "? (Y/N)");
-                    userConfirm = kb.nextLine();
-                    if (userConfirm.equalsIgnoreCase("y")) { // WHY IT NO WORK PHIL HELP ME
-                        deckList.remove((userInput - 1));
-                        System.out.println("Deck deleted");
-                    }
-                    else{
-                        System.out.println("Deck kept");
-                    }
-                    break;
-
-                case 5: break; //Ends Program
-
-                default: System.out.println("invalid entry");
+                default:
+                    System.out.println("invalid entry");
                     break;
             }
-
+            
+            // the menu prints and prompts user selected down here because it is a do loop rather than a while.
+            // this is so we can push the user straight into creating a deck without extraneously printing the menu options
             System.out.println("Enter an integer to choose a menu option");
             System.out.println("1. Create a deck.");
-            System.out.println("2. Study by term (top side).");
-            System.out.println("3. Study by definition (bottom side).");
-            System.out.println("4. Delete a deck.");
-            System.out.println("5. Exit Program.\n");
+            System.out.println("2. Select a deck.");
+            System.out.println("3. Exit Program.\n");
 
-            switchInt = kb.nextInt();
+            firstSwitchInt = kb.nextInt();
             kb.nextLine();
 
-        } while (switchInt != 5);
-
+        } while (firstSwitchInt != 3);
     }
 }
-
-/*
-class FlashCard {
-    String question;
-    String answer;
-
-    FlashCard (String q, String a) {
-            question = q;
-            answer = a;
-    }
-}
-*/
