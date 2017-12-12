00db7d39b40d23e17689d1f717271275cbb0482d
plempke <31676433+plempke@users.noreply.github.com>
2017-12-12 14:15:02 -0600

    Add files via upload
    Added Shuffle Deck and Show Deck

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -6,22 +6,23 @@ public class Deck {
 
     Scanner kbDeck = new Scanner(System.in);        // scanner for this class
     int deckSize;                                   // integer for the number of cards in the deck
-                                                    // this integer is not actually the number of cards in the deck but is equal to it instead.
-                                                    // basically, when the number of cards in the deck changes from the pop method, this number does not actually change and allows the for loops to run correctly
+    // this integer is not actually the number of cards in the deck but is equal to it instead.
+    // basically, when the number of cards in the deck changes from the pop method, this number does not actually change and allows the for loops to run correctly
     String deckName;                                // name of the deck
     String quest;                                   // top side of the deck (question)
     String ans;                                     // bottom side of the deck (answer)
     Queue<FlashCard> myDeck = new LinkedList<FlashCard>();  // queue to display cards in the order they are entered
 
-    public Deck(String name){   // constructor using the argument from line 33 in Main.java
+
+    public Deck(String name) {   // constructor using the argument from line 33 in Main.java
         deckName = name;
         System.out.println("Enter an integer deck size:");  // rather than ask the user if they want to enter a new card every time, we ask ahead of time what the deck size for faster, more streamlined deck creation
         deckSize = kbDeck.nextInt();
         kbDeck.nextLine();
         FlashCard[] fcArray = new FlashCard[deckSize];  // creates an array of flashcards the size of the user inputted value
 
-        for (int i=0; i<deckSize; i++) {    // creates one card and repeats an amount of times equal to the user input for deck size
-            System.out.println("\nCard " + (i+1));
+        for (int i = 0; i < deckSize; i++) {    // creates one card and repeats an amount of times equal to the user input for deck size
+            System.out.println("\nCard " + (i + 1));
             System.out.println("enter the top side of the card:");
             quest = kbDeck.nextLine();
             System.out.println("enter the bottom side of the card:");
@@ -31,10 +32,10 @@ public class Deck {
         }
     }
 
-    public void studyByQuestion(){  // runs through the queue displaying the top side of the card
+    public void studyByQuestion() {  // runs through the queue displaying the top side of the card
 
         FlashCard temp;
-        for (int i=0; i<deckSize; i++) {    // this is where deckSize's unchanging value is important as the deck's actual size DOES change during this loop
+        for (int i = 0; i < deckSize; i++) {    // this is where deckSize's unchanging value is important as the deck's actual size DOES change during this loop
             temp = myDeck.poll();   // pulls the card out of the front of the queue (this removes it from the queue)
             System.out.println("Top:");
             System.out.println(temp.question);
@@ -46,22 +47,50 @@ public class Deck {
         }
     }
 
-    public void studyByAnswer(){    // runs through the queue displaying the bottom side of the card
+    public void studyByAnswer() {    // runs through the queue displaying the bottom side of the card
 
         FlashCard temp;
-        for (int i=0; i<deckSize; i++) {    // this is where deckSize's unchanging value is important as the deck's actual size DOES change during this loop
+        for (int i = 0; i < deckSize; i++) {    // this is where deckSize's unchanging value is important as the deck's actual size DOES change during this loop
             temp = myDeck.poll();   // pulls the card out of the front of the queue (this removes it from the queue)
             System.out.println("Bottom:");
             System.out.println(temp.answer);
             kbDeck.nextLine();
             System.out.println("Top:");
-            System.out.println(temp.question+ "\n");
+            System.out.println(temp.question + "\n");
             myDeck.add(temp);   // re-adds the pulled card to the back of the queue
         }
     }
 
+    public void showDeck() {
+        FlashCard temp;
 
-}
+        for (int i = 0; i < deckSize; i++) {
+            temp = myDeck.poll();
+            System.out.println("Card " + (i+1));
+            System.out.println(temp.question);
+            System.out.println(temp.answer);
+            myDeck.add(temp);
+
+        }
+    }
+
+    public void shuffleDeck() {
+        FlashCard[] shuffleDeck = new FlashCard[deckSize];
+        FlashCard temp;
+
+        for (int i = 0; i < deckSize; i++) {     //pulls queue and stores in array
+            shuffleDeck[i] = myDeck.poll();
+        }
+        for (int i = deckSize - 1; i >= 1; i--) {       //shuffles the created array
+            int n = (int) Math.floor(Math.random() * (i));
+            temp = shuffleDeck[i];
+            shuffleDeck[i] = shuffleDeck[n];
+            shuffleDeck[n] = temp;
+        }
+        for (int i = 0; i < deckSize; i++) {     //post shuffle
+            myDeck.add(shuffleDeck[i]);
+        }
+    }
 
 
     /*
@@ -89,13 +118,13 @@ public class Deck {
     */
 
 
+    class FlashCard {       // very simple custom object that just holds two strings representing two sides of a flashcard
+        String question;    // top side
+        String answer;      // bottom side
 
-class FlashCard {       // very simple custom object that just holds two strings representing two sides of a flashcard
-    String question;    // top side
-    String answer;      // bottom side
-
-    FlashCard (String q, String a) {    // constructor from line 29 of this file
-        question = q;
-        answer = a;
+        FlashCard(String q, String a) {    // constructor from line 29 of this file
+            question = q;
+            answer = a;
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,11 +17,8 @@ public class Main {
         String nameTemp;                            // temporary string for the name of the new deck in deck creation
 
 
-
         System.out.println("Welcome to this Flashcard Application!");
         System.out.println("Start by creating a deck.");
-
-
 
 
         do {    // This is a do statement so that we can push the user to create a deck right off the bat.
@@ -60,51 +57,56 @@ public class Main {
 
 
 
-                                // beginning of the sub-menu for the individual deck
+                        // beginning of the sub-menu for the individual deck
                         do {    // this is a do rather than a while so that when the user selects 5 to return to the main menu, this loop will still run correctly upon returning
 
-                            System.out.println("\n1. Study " + selectedDeck.deckName + " by question");
-                            System.out.println("2. Study " + selectedDeck.deckName + " by answer");
-                            System.out.println("3. Shuffle " + selectedDeck.deckName);
-                            System.out.println("4. Delete " + selectedDeck.deckName);
-                            System.out.println("5. Return to main menu\n");
+                            System.out.println("\n1. Show cards in " + selectedDeck.deckName);
+                            System.out.println("2. Study " + selectedDeck.deckName + " by question");
+                            System.out.println("3. Study " + selectedDeck.deckName + " by answer");
+                            System.out.println("4. Shuffle " + selectedDeck.deckName);
+                            System.out.println("5. Delete " + selectedDeck.deckName);
+                            System.out.println("6. Return to main menu\n");
 
                             secondSwitchInt = kb.nextInt();
                             kb.nextLine();
 
                             switch (secondSwitchInt) {
                                 case 1:
+                                    //list cards in deck
+                                    selectedDeck.showDeck();
+                                    break;
+
+                                case 2:
                                     //Study by question
                                     selectedDeck.studyByQuestion(); // runs the studyByQuestion method
                                     break;
 
-                                case 2:
+                                case 3:
                                     //Study by answer
                                     selectedDeck.studyByAnswer();   // runs the studyByAnswer method
                                     break;
 
-                                case 3:
+                                case 4:
                                     //Shuffle Deck
-                                    //selectedDeck.shuffle
-                                    System.out.println("Shuffle is not yet implemented");   // Debug purposes
+                                    selectedDeck.shuffleDeck();
+                                    System.out.println("Your deck has been shuffled");   // Debug purposes
                                     break;
 
-                                case 4:
+                                case 5:
                                     //Delete Deck
                                     System.out.println("Are you sure you wish to delete " + selectedDeck.deckName + "? (Y/N)"); // The confirmation inquiry exists for the case that the user hits 4 on accident (user health)
                                     userConfirm = kb.nextLine();
                                     if (userConfirm.equalsIgnoreCase("y")) {
                                         deckList.remove((userInput - 1));   // deleting the deck stored in selectedDeck does not actually remove it from the list as selectedDeck is just copy of the deck from the list
-                                                                            // one benefit of using a dummy deck for these operations is that the original order is stored in the event that the user wants to return to the original deck order after shuffling.
+                                        // one benefit of using a dummy deck for these operations is that the original order is stored in the event that the user wants to return to the original deck order after shuffling.
                                         System.out.println("Deck deleted.");    //debug purposes
-                                        secondSwitchInt = 5;
                                     }
                                     else {
                                         System.out.println("Deck not deleted.");    //debug purposes
                                     }
                                     break;
 
-                                case 5:
+                                case 6:
                                     // return to main menu
                                     // it's more important that secondSwitchInt = 5 for the while statement below than the actual case statement here
                                     break;
@@ -130,7 +132,7 @@ public class Main {
                     System.out.println("invalid entry");
                     break;
             }
-            
+
             // the menu prints and prompts user selected down here because it is a do loop rather than a while.
             // this is so we can push the user straight into creating a deck without extraneously printing the menu options
             System.out.println("Enter an integer to choose a menu option");
